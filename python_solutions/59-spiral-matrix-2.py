c6a96673f64e10831f9f612d66f3018c08d6708d
Leon Li <yunieyuna@163.com>
2019-12-13 17:38:33 +0900

    Create 59-spiral-matrix-2.py

---
+++ python_solutions/59-spiral-matrix-2.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/spiral-matrix-ii/
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        l, r, t, b = 0, n - 1, 0, n - 1
+        mat = [[0 for _ in range(n)] for _ in range(n)]
+        num, tar = 1, n * n
+        while num <= tar:
+            for i in range(l, r + 1): # left to right
+                mat[t][i] = num
+                num += 1
+            t += 1
+            for i in range(t, b + 1): # top to bottom
+                mat[i][r] = num
+                num += 1
+            r -= 1
+            for i in range(r, l - 1, -1): # right to left
+                mat[b][i] = num
+                num += 1
+            b -= 1
+            for i in range(b, t - 1, -1): # bottom to top
+                mat[i][l] = num
+                num += 1
+            l += 1
+        return mat
+
+"""
+Runtime: 116 ms, faster than 6.00% of Python3 online submissions for Spiral Matrix II.
+Memory Usage: 32.4 MB, less than 9.09% of Python3 online submissions for Spiral Matrix II.
+"""
