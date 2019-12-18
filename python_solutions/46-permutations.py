c9e681d6164419800fbc23ae4b0c36d51dbbb978
Leon Li <yunieyuna@163.com>
2019-12-18 10:44:03 +0900

    Update 46-permutations.py

--- python_solutions/46-permutations.py
+++ python_solutions/46-permutations.py
@@ -19,3 +19,20 @@ class Solution:
                 ans.append([nums[i]] + j)
         return ans
         
+# method 3: Recursion
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         res = []
+#         def backtrack(nums, tmp):
+#             if not nums:
+#                 res.append(tmp)
+#                 return
+#             for i in range(len(nums)):
+#                 backtrack(nums[:i] + nums[i+1:], tmp + [nums[i]])
+#         backtrack(nums, [])
+#         return res
+
+"""
+Runtime: 32 ms, faster than 98.23% of Python3 online submissions for Permutations.
+Memory Usage: 12.9 MB, less than 100.00% of Python3 online submissions for Permutations.
+"""
