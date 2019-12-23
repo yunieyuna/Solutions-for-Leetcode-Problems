c14abfb70186402706ce3c12734e0447c6edac46
Leon Li <yunieyuna@163.com>
2019-12-23 10:30:49 +0900

    Create 709-to-lower-case.py

---
+++ python_solutions/709-to-lower-case.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/to-lower-case/
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
+        
+"""
+Runtime: 28 ms, faster than 79.90% of Python3 online submissions for To Lower Case.
+Memory Usage: 12.7 MB, less than 100.00% of Python3 online submissions for To Lower Case.
+"""
