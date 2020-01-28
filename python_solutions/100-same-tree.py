a0df0c9e40fdadd9a0a8bf85d09cb0ab213ae772
Leon Li <yunieyuna@163.com>
2020-01-29 00:14:38 +0900

    Create 100-same-tree.py

---
+++ python_solutions/100-same-tree.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/same-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+
+        # one of node is None
+        if not p or not q:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+"""
+Runtime: 32 ms, faster than 32.31% of Python3 online submissions for Same Tree.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Same Tree.
+"""
