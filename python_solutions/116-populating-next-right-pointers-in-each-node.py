f3d2b40a4731e0f8be1e564fd1ec2cb7510797d5
Leon Li <yunieyuna@163.com>
2020-01-31 11:04:23 +0900

    Create 116-populating-next-right-pointers-in-each-node.py

---
+++ python_solutions/116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        cur  = root
+        next = root.left
+
+        while cur.left :
+            cur.left.next = cur.right
+            if cur.next:
+                cur.right.next = cur.next.left
+                cur = cur.next
+            else:
+                cur = next
+                next = cur.left
+                
+        return root
+        
+"""
+Runtime: 60 ms, faster than 86.43% of Python3 online submissions for Populating Next Right Pointers in Each Node.
+Memory Usage: 14.2 MB, less than 100.00% of Python3 online submissions for Populating Next Right Pointers in Each Node.
+"""
