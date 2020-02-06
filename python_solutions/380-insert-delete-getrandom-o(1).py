204ac0edc034e3f92d2a3a6e756a3e10cf1f2b6d
Leon Li <yunieyuna@163.com>
2020-02-06 11:14:16 +0900

    Create 380-insert-delete-getrandom-o(1).py

---
+++ python_solutions/380-insert-delete-getrandom-o(1).py
@@ -0,0 +1,53 @@
+# https://leetcode.com/problems/insert-delete-getrandom-o1/
+
+from random import choice
+
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.dict = {}
+        self.list = []
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.dict:
+            return False
+        self.dict[val] = len(self.list)
+        self.list.append(val)
+        return True
+        
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.dict:
+            last_element, idx = self.list[-1], self.dict[val]
+            self.list[idx], self.dict[last_element] = last_element, idx
+            self.list.pop()
+            del self.dict[val]
+            return True
+        return False
+        
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return choice(self.list)
+        
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
+
+"""
+Runtime: 96 ms, faster than 84.89% of Python3 online submissions for Insert Delete GetRandom O(1).
+Memory Usage: 16.8 MB, less than 83.33% of Python3 online submissions for Insert Delete GetRandom O(1).
+"""
